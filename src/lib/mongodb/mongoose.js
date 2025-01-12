4e8ebe536fdcfe4aec6470423f5305a897056356
Sudip Sharma <sudpokhara826@gmail.com>
2025-01-12 14:29:33 +0545

    Correct to the db commit

--- src/lib/mongodb/mongoose.js
+++ src/lib/mongodb/mongoose.js
@@ -1,17 +1,38 @@
 import mongoose from 'mongoose';
+
 let initialized = false;
+
 export const connect = async () => {
-  mongoose.set('strictQuery', true);
   if (initialized) {
     console.log('Already connected to MongoDB');
     return;
   }
+
+  // Set mongoose options to avoid deprecated warnings
+  mongoose.set('strictQuery', false); // depending on your use case, you might want to set it to false
+  
+  // Check if MONGODB_URI is defined
+  if (!process.env.MONGODB_URI) {
+    console.log('MONGODB_URI is not set in environment variables');
+    return;
+  }
+
+  console.log('Connecting to MongoDB', process.env.MONGODB_URI);
+
   try {
+    // Check if the database connection is already established
+    if (mongoose.connection.readyState === 1) {
+      console.log('Already connected to MongoDB');
+      return;
+    }
+
+    // Connect to MongoDB
     await mongoose.connect(process.env.MONGODB_URI, {
       dbName: 'next-blog',
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
+
     console.log('Connected to MongoDB');
     initialized = true;
   } catch (error) {
